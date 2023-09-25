88cb3c95cdc9d140d86468ae0c46582307d00f53
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2023-09-25 20:17:10 +0700

    socketserver

--- src/controller/CRUD.js
+++ src/controller/CRUD.js
@@ -1,37 +1,86 @@
 const { ObjectId } = require("mongodb");
 
-// VIEW DATA
-async function viewData(db, collectionName) {
-    const collection = db.collection(collectionName);
-    const data = await collection.find().toArray();
-  
-    return data;
-  }
+async function updateManyData(client) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
 
-
-  // UPDATE
-  async function updateOneDataAndReturn(client, collectionName, id, dataUpdate) {
-    const collection = client.collection(collectionName);
-  
-    const result = await collection.findOneAndUpdate(
-      { _id: new ObjectId(id) },
-      { $set: dataUpdate }
+    .updateMany(
+      { property_type: { $exists: false } },
+      { $set: { property_type: "Unknown" } }
     );
-  
-    const resultFinal = await collection.findOne({ _id: new ObjectId(id) });
-  
-    if (result) {
-      console.log("new Data from Update : ", resultFinal);
-      return resultFinal;
-    } else {
-      console.log("Đéo có data");
-    }
-  
-    return result.value;
+
+  console.log(`Có ${result.matchedCount} tài liệu khớp với tiêu chí truy vấn.`);
+  console.log(`Có ${result.modifiedCount} tài liệu đã được cập nhật.`);
+}
+
+async function updateOneData(client, collectionName, id, dataUpdate) {
+  const collection = client.collection(collectionName);
+
+  const result = await collection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: dataUpdate }
+  );
+
+  if (result) {
+    console.log("new Data from Update : ", result);
+    return result;
+  } else {
+    console.log("Đéo có data");
   }
 
-  // FIND
+  return result.value;
+}
 
+async function updateOneDataAndReturn(client, collectionName, id, dataUpdate) {
+  const collection = client.collection(collectionName);
+
+  const result = await collection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: dataUpdate }
+  );
+
+  const resultFinal = await collection.findOne({ _id: new ObjectId(id) });
+
+  if (result) {
+    console.log("new Data from Update : ", resultFinal);
+    return resultFinal;
+  } else {
+    console.log("Đéo có data");
+  }
+
+  return result.value;
+}
+
+// DELETE
+
+async function deleteAll(client) {
+  const result = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .deleteMany({});
+
+  console.log(`Đã xóa ${result.deletedCount} bản ghi.`);
+}
+
+async function deleteOneData(client, collectionName, id) {
+  try {
+    const collection = client.collection(collectionName);
+
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    if (result.deletedCount > 0) {
+      console.log(`Delete done`);
+      return result
+    } else {
+      console.log(`Đéo ra data để xóa`);
+    }
+  } catch (error) {
+    console.error("Lỗi khi xóa bản ghi:", error);
+  }
+}
+
+// FIND
 async function findOneName(client, collectionName, Name) {
   try {
     const collection = client.collection(collectionName);
@@ -53,7 +102,7 @@ async function findById(client, collectionName, id) {
   try {
     const collection = client.collection(collectionName);
 
-    const result = await collection.findOne({ _id: id });
+    const result = await collection.findOne({ _id: new ObjectId(id) });
 
     if (result) {
       console.log("Yes from findOneById");
@@ -66,10 +115,95 @@ async function findById(client, collectionName, id) {
   }
 }
 
-  module.exports = {
-    viewData,
-    updateOneDataAndReturn,
-    findOneName, 
-    findById
+async function findUserId(client, collectionName, userId1, userId2) {
+  try {
+    const collection = client.collection(collectionName);
 
+    const result = await collection.findOne({ $or: [{ userX: userId1, userY: userId2 }, { userY: userId1, userX: userId2 }] });
+
+    if (result) {
+      console.log("Yes from findOneById");
+      return result;
+    } else {
+      console.log("No from findOneById");
+    }
+  } catch (error) {
+    console.error("ERROR", error);
+  }
+}
+
+async function findVsManyKey(
+  client,
+  { name = null, gmail = null, numberResult = Number.MAX_SAFE_INTEGER } = {}
+) {
+  const collection = client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews");
+
+  const query = {
+    name: { $eq: name },
+    mail: { $eq: gmail },
   };
+
+  const sort = { last_review: -1 };
+
+  const limit = numberResult;
+
+  const cursor = collection.find(query).sort(sort).limit(limit);
+
+  const result = await cursor.toArray();
+
+  if (result.length > 0) {
+    result.forEach((re) => {
+      console.log("new Name:", re);
+    });
+  } else {
+    console.log("Đéo Có Data");
+  }
+}
+
+// CREATE DATA
+
+async function createManyData(client, Oject) {
+  const newData = await client
+    .db("sample_airbnb")
+    .collection("listingsAndReviews")
+    .insertMany(Oject);
+
+  console.log("new data : ", newData);
+}
+
+const createOneData = async (db, collectionName, document) => {
+  const collection = db.collection(collectionName);
+
+  const result = await collection.insertOne(document);
+  console.log(
+    `Inserted ${result.insertedCount} documents into ${collectionName}`
+  );
+
+  return result;
+};
+
+// VIEW DATA
+async function viewData(db, collectionName) {
+  const collection = db.collection(collectionName);
+  const data = await collection.find().toArray();
+
+  return data;
+}
+
+
+module.exports = {
+  updateManyData,
+  updateOneData,
+  updateOneDataAndReturn,
+  deleteAll,
+  deleteOneData,
+  findOneName,
+  findById,
+  findVsManyKey,
+  createManyData,
+  createOneData,
+  viewData,
+  findUserId
+};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,124 @@
-const express = require('express');
+require("dotenv").config();
+import { createServer } from "http";
 import configViewEngine from './configs/configsViewEngine';
-import initWebRoute from './views/web';
+import CRUD from './controller/CRUD'
+const { MongoClient, ObjectId } = require("mongodb");
+const express = require('express');
+const morgan = require('morgan')
+const cors = require("cors");
 const bodyParser = require('body-parser');
-
 const app = express();
-const port = 4040
-
-app.use(bodyParser.json());
-
-configViewEngine(app);
+const server =  createServer(app);
+// app.use(morgan('combined'))
+// app.use(bodyParser.json());
+// app.use(cors()); 
 
 // view enginer
 configViewEngine(app)
 
-// router 
-initWebRoute(app)
+// Connection URL User
+const urlUser = `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.PASSWORD_DATABASE}@cluster0.mrz3qxp.mongodb.net/${process.env.DATABASE_USER}?retryWrites=true&w=majority`;
+const clientUser = new MongoClient(urlUser);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Connection URL Post
+const urlPost = `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.PASSWORD_DATABASE}@cluster0.mrz3qxp.mongodb.net/${process.env.DATABASE_POST}?retryWrites=true&w=majority`;
+const clientPost = new MongoClient(urlPost);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// Connection URL Post
+const urlMessage = `mongodb+srv://${process.env.DATABASE_NAME}:${process.env.PASSWORD_DATABASE}@cluster0.mrz3qxp.mongodb.net/${process.env.DATABASE_MESAGE}?retryWrites=true&w=majority`;
+const clientMessage = new MongoClient(urlMessage);
+
+// DotENV
+const port = process.env.PORT;
+const dbName = process.env.DATABASE_USER_NAME;
+const dataUser = process.env.DATABASE_USER;
+const dataPost = process.env.DATABASE_POST;
+const dataMessage = process.env.DATABASE_MESAGE;
+
+// Database Name
+const dbUser = clientUser.db(dbName);
+const dbPost = clientPost.db(dbName);
+const dbMessage = clientMessage.db(dbName);
+
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: `${process.env.URL_CLIENT}`,
+    methods: ["GET", "POST"]
+  }
+});
+
+const onlineUsers = [];
+const notificationUsers = [];
+io.on("connection", (socket) => {
+  console.log(socket.id, 954);
+  socket.on("joinRoom", (data) => {
+    console.log("Data received", data, 979);
+    socket.join(data);
+  });
+
+  //Notification
+  socket.on("Notification", async (data) => {
+    console.log(data, 852);
+    notificationUsers.push(data.userId);
+    io.emit("NotificationData", notificationUsers);
+    let index = notificationUsers.indexOf(data.userId);
+    if (index !== -1) {
+      notificationUsers.splice(index, 1);
+    }
+    if (data.content == "Có Người Nhắn Tin ") {
+      console.log(12);
+      const User = await CRUD.findById(dbUser, dataUser, data.userId);
+      const arrayConnect = User.connect;
+      arrayConnect.push(data);
+      const UpdateUser = await CRUD.updateOneDataAndReturn(
+        dbUser,
+        dataUser,
+        data.userId,
+        { connect: arrayConnect }
+      );
+    }
+  });
+  // Check Offline
+  socket.on("checkUserOffline", (data) => {
+    // onlineUsers.slice(0,0)
+    let index = onlineUsers.indexOf(data.myId);
+    if (index !== -1) {
+      onlineUsers.splice(index, 1);
+    }
+    console.log(onlineUsers, 942);
+    io.emit("Data check User Online", onlineUsers);
+  });
+  // Check Online
+  socket.on("checkUserOnline", (data) => {
+    // onlineUsers.slice(0,0)
+    onlineUsers.push(data.myId);
+    io.emit("Data check User Online", onlineUsers);
+  });
+  // Message
+  socket.on("DataMessage", async (data) => {
+    const findMessage = await CRUD.findById(
+      dbMessage,
+      dataMessage,
+      data.roomId
+    );
+    const OldMessage = findMessage.message;
+    io.to(data.roomId).emit("ServerResponse", { data, OldMessage });
+    OldMessage.push(data);
+    const updateMessage = await CRUD.updateOneDataAndReturn(
+      dbMessage,
+      dataMessage,
+      data.roomId,
+      { message: OldMessage }
+    );
+  });
+});
+// io.listen(4000);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// 404 
+app.use((req,res) => {
+  return res.send('Cút')
 })
